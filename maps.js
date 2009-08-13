86cf2cd3e24b5db52187ee35322c4322d28d6fe2
John Huddleston <huddlej@kexp.(none)>
2009-08-12 17:20:20 -0700

    added GoogleMap class to represent all google map markers and settings

---
+++ maps.js
@@ -0,0 +1,19 @@
+function GoogleMap(map_id) {
+    // Represents a Google Map instance including all markers displayed on the
+    // map and the data in the marker info windows.
+    var map = GMap2($("#" + map_id));
+    var manager = MarkerManager(this.map);
+    var markers = [];
+
+    var createMarker = function(latitude, longitude, marker_options) {
+        // Creates a new marker using the given latitude and longitude.
+        var latlng = GLatLng(latitude, longitude);
+        var marker = new GMarker(latlng, marker_options);
+        this.markers.push(marker);
+    }
+
+    var displayMarkers = function() {
+        this.manager.addMarkers(this.markers, 3, 10);
+        this.manager.refresh();  
+    }
+}
